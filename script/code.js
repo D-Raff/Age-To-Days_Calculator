b0d50bb32eddde2ebdbd7333f1572abefa6b6410
D-Raff <damonraffels@gmail.com>
2023-11-27 10:19:59 +0200

    completed js

--- script/code.js
+++ script/code.js
@@ -1,1 +1,25 @@
 // functionality
+
+// button variable
+const btnCalc = document.querySelector('[data-calc]')
+
+// function to 
+function Convert () {
+    // variable for input
+    let age = document.querySelector('[data-inp]')
+    
+    //variable for the output
+    let output = document.querySelector('[data-res]')
+
+    // varaiable for result
+    let res = +age.value * 365
+    
+    // append to output
+    if (+age.value < 0 ){
+        output.textContent = "please enter a valid age"
+    }else{
+        output.textContent = `${res} is your age in days!`
+    }
+    
+}
+btnCalc.addEventListener('click', Convert)
